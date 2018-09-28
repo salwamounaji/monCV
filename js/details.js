d8dfc2b0c4a638b4ee3feb0dab07fb9aed7c6e82
MOUNAJI SALWA <salwa.mounaji1@gmail.com>
2018-09-28 14:45:29 +0200

    TP-JS-Q3

--- js/details.js
+++ js/details.js
@@ -31,13 +31,29 @@ else{
     }
 }
 
-var z = 0;
-
-function progressif(){
-	var mydiv = document.getElementsByClassName('ajoutcom')[0];
-	if(mydiv.style.height != '150px'){
-		mydiv.style.height = z + 'px';     
-		z += 10;
-		setTimeout('progressif()', 30);
+function SizeProgress(multiple) {
+	var divid=document.getElementById(multiple);
+	var list = document.getElementsByClassName("listeformation");
+	[].forEach.call(list, function (i) {i.classList.remove("listeformation");
+	if(i!==divid)
+	i.style.display="none";
+	});
+	if(divid.style.display == 'none'){
+	divid.classList.add("listeformation");
+	divid.style.display='inline';
 	}
+	else{
+	divid.style.display='none';
+	}	
+	var myAwesomeVar = 2;
+	//let timerId = setInterval(function (){var liste = document.getElementsByClassName("p2");liste[0].style.fontSize= myAwesomeVar++ + "px";}, 50);
+
+	//let timerId = setInterval(function (){[].forEach.call(list, function (i) {i.style.fontSize= myAwesomeVar + "px";myAwesomeVar++;})}, 150);
+
+	let timerId = setInterval(function (){var i=0;list[i].style.fontSize= myAwesomeVar++ + "px";i++;}, 70);
+	// after 2 seconds stop
+	setTimeout(() => { clearInterval(timerId);}, 1000);
 }
+
+
+
