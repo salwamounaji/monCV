4a3cbe6a6bbf9316f6c002a69b8837d792565055
MOUNAJI SALWA <salwa.mounaji1@gmail.com>
2018-10-02 14:32:38 +0200

    UN commit

--- js/details.js
+++ js/details.js
@@ -31,27 +31,31 @@ else{
     }
 }
 
-function SizeProgress(multiple) {
-	var divid=document.getElementById(multiple);
-	var list = document.getElementsByClassName("listeformation");
-	[].forEach.call(list, function (i) {i.classList.remove("listeformation");
-	if(i!==divid)
-	i.style.display="none";
-	});
-	if(divid.style.display == 'none'){
-	divid.classList.add("listeformation");
-	divid.style.display='inline';
-	}
-	else{
-	divid.style.display='none';
-	}	
-	var myAwesomeVar = 2;
-	//let timerId = setInterval(function (){var liste = document.getElementsByClassName("p2");liste[0].style.fontSize= myAwesomeVar++ + "px";}, 50);
+function SizeProgress(x) {
 
-	//let timerId = setInterval(function (){[].forEach.call(list, function (i) {i.style.fontSize= myAwesomeVar + "px";myAwesomeVar++;})}, 150);
+	var divid=document.getElementById(x);
 
-	let timerId = setInterval(function (){var i=0;list[i].style.fontSize= myAwesomeVar++ + "px";i++;}, 70);
-	// after 2 seconds stop
+		var visibles = document.getElementsByClassName("visible");
+
+		[].forEach.call(visibles, function (i) {i.classList.remove("visible");
+			if(i!==divid)
+			i.style.display="none";
+});
+
+		if(divid.style.display == 'none'){
+		divid.classList.add("visible");
+		divid.style.display='inline';
+}
+		else{
+
+		divid.style.display='none';
+		}
+
+	var y = 2;
+	
+
+	let timerId = setInterval(function (){var i=0;visibles[i].style.fontSize= y++ + "px";i++;}, 70);
+
 	setTimeout(() => { clearInterval(timerId);}, 1000);
 }
 
